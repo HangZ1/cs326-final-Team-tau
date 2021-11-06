f2b44221c1fd69397d830c55a31d3a6d29387597
Hang Zheng <hangzheng@umass.edu>
2021-11-06 12:53:05 -0400

    hang

---
+++ Client/Output_page.js
@@ -0,0 +1,7 @@
+document.getElementById("cart").addEventListener('click', () => {
+    window.location.href = 'ShoppingCart.html';
+})
+
+document.getElementById("redo").addEventListener("click", () => {
+    window.location.href = "Select.html"
+})
