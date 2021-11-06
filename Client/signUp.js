8065ec16470441acf87695c59a09162c10573d99
Your Name <you@example.com>
2021-11-06 17:25:40 -0400

    sing up finish

--- Client/signUp.js
+++ Client/signUp.js
@@ -6,7 +6,6 @@ document.getElementById('signIn').addEventListener('click',() => {
     window.location.href = 'Signin_page.html';
 })
 
-
 document.getElementById('signUp').addEventListener('click',()=>{
     let un = document.getElementById('userName').value;
     let pw = document.getElementById('passWord').value;
@@ -16,7 +15,10 @@ document.getElementById('signUp').addEventListener('click',()=>{
             alert('The password entered does not match');
         }
         else{
-            register(un,pw);
+            if (register(un, pw)) {
+                alert("sign up successfully");
+                window.location.href = 'Signin_page.html';
+            }
         }
     }
     else{
@@ -25,8 +27,22 @@ document.getElementById('signUp').addEventListener('click',()=>{
 })
 
 async function register(un, pw){
-    const response = await fetch('http://localhost:8080//Register?name=' + un +'&password=' + pw);
+    const response = await fetch('http://localhost:8080/Register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({
+            name: un ,
+            password: pw
+        })
+    });
     if(response.ok){
-        return await response.text();
+        const responseTEXT = await response.text();
+        if(responseTEXT === "Regist") {
+            return true;
+        }else{
+            alert(responseTEXT);
+        }
     }
 }
